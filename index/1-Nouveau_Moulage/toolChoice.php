601933bc5e5e6144708018eba003e1e63e117892
Franck-Dev <52229344+Franck-Dev@users.noreply.github.com>
2020-05-29 15:10:37 +0200

    Update toolChoice.php
    Rewrite Database connector

--- index/1-Nouveau_Moulage/toolChoice.php
+++ index/1-Nouveau_Moulage/toolChoice.php
@@ -4,7 +4,7 @@
     <select id="programs" class='programs' name='programs' data-target='#tool' data-source='/Tuto/index/1-Nouveau_Moulage/script2.php?types=tool&filter=$prog'>
         <option>Selectionner un programme avion</option>
         <?php
-            $bdd = new PDO('mysql:host=localhost:3306;dbname=traca;charset=utf8', 'root', '');
+            $bdd = new PDO('', '', '');
             $sql= 'SELECT * FROM t_program';
             $programs = $bdd->query($sql);
             foreach ($programs as $program) {?>
